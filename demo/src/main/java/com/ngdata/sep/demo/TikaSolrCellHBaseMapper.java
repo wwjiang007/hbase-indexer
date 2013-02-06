c831e8403629ab62706935a41f38216c99f7c753
Bruno Dumon <bruno@ngdata.com>
2013-02-06 17:17:02 +0100

    Don't index payload fields in solr

--- demo/src/main/java/com/ngdata/sep/demo/TikaSolrCellHBaseMapper.java
+++ demo/src/main/java/com/ngdata/sep/demo/TikaSolrCellHBaseMapper.java
@@ -80,6 +80,7 @@ public class TikaSolrCellHBaseMapper {
         cellParams.put("uprefix", "ignored_");
         cellParams.put("fmap.row", "id");
         cellParams.put("fmap.content", "ignored_field");
+        cellParams.put("fmap.payload", "ignored_field");
         SolrContentHandler handler = new SolrContentHandler(metadata, new MapSolrParams(cellParams), schema);
 
         // Convert the HBase Result to an InputStream to pass it to the Tika parser
