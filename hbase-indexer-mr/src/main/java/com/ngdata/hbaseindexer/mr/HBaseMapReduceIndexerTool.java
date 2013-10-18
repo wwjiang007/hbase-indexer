851ff3d0954de2a8c49bf940fb5a418434dd8460
Gabriel Reid <gabrielr@ngdata.com>
2013-10-18 16:35:24 +0200

    Set job name properly for direct-write job

--- hbase-indexer-mr/src/main/java/com/ngdata/hbaseindexer/mr/HBaseMapReduceIndexerTool.java
+++ hbase-indexer-mr/src/main/java/com/ngdata/hbaseindexer/mr/HBaseMapReduceIndexerTool.java
@@ -17,6 +17,9 @@ package com.ngdata.hbaseindexer.mr;
 
 import java.io.IOException;
 
+import org.apache.solr.hadoop.TreeMergeMapper;
+import org.apache.solr.hadoop.Utils;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
@@ -76,6 +79,7 @@ public class HBaseMapReduceIndexerTool extends Configured implements Tool {
         conf.setBoolean(HBaseIndexerMapper.INDEX_DIRECT_WRITE_CONF_KEY, hbaseIndexingOpts.isDirectWrite());
         
         Job job = Job.getInstance(getConf());
+        job.setJobName(getClass().getName() + "/" + HBaseIndexerMapper.class.getSimpleName());
         job.setJarByClass(HBaseIndexerMapper.class);
         job.setUserClassesTakesPrecedence(true);
         
